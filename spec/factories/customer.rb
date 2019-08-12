39b729c4ebda9880908d5b0ffc879f1a94281e11
= <=>
2019-08-12 12:36:01 -0300

    FactoryBot

---
+++ spec/factories/customer.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+    factory :customer do
+        name "Tiago Lima"
+        email "tiago@gmail.com"
+    end
+end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,11 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  fixtures :all
+  #fixtures :all
+
+  #it 'Create a Customer' do
+    #customer = customers(:tiago)
+
+    #expect(customer.full_name).to eq("Sr. Tiago Lima")
+  #end
 
   it 'Create a Customer' do
-    customer = customers(:tiago)
-
+    customer = create(:customer)
     expect(customer.full_name).to eq("Sr. Tiago Lima")
   end
 end
