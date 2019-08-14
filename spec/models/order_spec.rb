b083d08d36642b9cb7e569c11cc35cd8c7dbba89
= <=>
2019-08-14 17:51:16 -0300

    Create List

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,9 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  it 'Tem 1 pedido' do
-    
+  it 'Tem 1 pedido' do    
     order = create(:order)    
     expect(order.customer).to be_kind_of(Customer)
+  end
+
+  it 'Tem 3 pedidos' do
+    orders = create_list(:order, 3, description: "teste")
+    expect(orders.count).to eq(3)
+    expect(orders[0].description).to eq("teste")
   end
 end
