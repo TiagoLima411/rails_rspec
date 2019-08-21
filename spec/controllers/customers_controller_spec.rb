1c295f9038e16c952a500d2c5e4e7c67730fec5c
= <=>
2019-08-21 17:32:23 -0300

    Testando controllers(entradas do usuário)

--- spec/controllers/customers_controller_spec.rb
+++ spec/controllers/customers_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe CustomersController, type: :controller do
   
-  describe 'As  a ggust' do
+  describe 'As a Guest' do
     context '#index' do
 
       it "returns a success response" do    
@@ -30,26 +30,31 @@ RSpec.describe CustomersController, type: :controller do
   end
 
   describe 'As Logged Member' do
-    
-      it "responds a 200 response" do    
-        member = create(:member)
-        customer = create(:customer)
-    
-        sign_in member
-    
-        get :show, params: {id: customer.id}
-        expect(response).to have_http_status(200)
-      end
 
-      it "render a show template" do    
-        member = create(:member)
-        customer = create(:customer)
-    
-        sign_in member
-    
-        get :show, params: {id: customer.id}
-        expect(response).to render_template(:show)
-      end
+    before do
+      @member = create(:member)
+      @customer = create(:customer)
+    end
+
+    it 'with valid attributes' do
+      customer_params = attributes_for(:customer)
+      sign_in @member
+      expect{
+        post :create, params: { customer: customer_params }
+      }.to change(Customer, :count).by(1)   
+    end
+
+    it "responds a 200 response" do    
+      sign_in @member
+      get :show, params: {id: @customer.id}
+      expect(response).to have_http_status(200)
+    end
+
+    it "render a show template" do    
+      sign_in @member
+      get :show, params: {id: @customer.id}
+      expect(response).to render_template(:show)
+    end
     
   end
 
